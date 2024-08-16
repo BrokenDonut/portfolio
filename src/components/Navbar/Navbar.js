e2d9dd69984446c9ed8791201629cd2046a01388
RonaldoChulluncuy <ronaldochulluncuy@gmail.com>
2024-08-16 15:33:16 -0500

    2.2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import './navbar.css'; // Asegúrate de importar el archivo CSS
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -21,21 +22,28 @@ const Navbar = () => {
         };
     }, []);
 
+    const handleMenuClick = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+        <nav className={`navbar ${scrolled ? 'scrolled' : ''} ${menuOpen ? 'menu-open' : ''}`}>
             <div className={`container-navbar ${scrolled ? 'scrolled' : ''}`}>
                 <div className="tittle">
                     <div className="tittle-decor">
                         <h1>Ronaldo</h1><h1 className="punto">.</h1>
                     </div>
                 </div>
-                <ul className="option">
-                    <a href="#perfil">Perfil</a>
-                    <a href="#about">Habilidades</a>
-                    <a href="#work">Proyectos</a>
-                    <a href="#contact">Contacto</a>
+                <ul className={`option ${menuOpen ? 'show' : ''}`}>
+                    <a className='option-scroll' href="#perfil">Perfil</a>
+                    <a className='option-scroll' href="#about">Habilidades</a>
+                    <a className='option-scroll' href="#work">Proyectos</a>
+                    <a className='option-scroll' href="#contact">Contacto</a>
                 </ul>
-                <button className="burger-menu">
+                <button 
+                    className={`burger-menu ${menuOpen ? 'open' : ''}`} 
+                    onClick={handleMenuClick}
+                >
                     <div className="bar"></div>
                     <div className="bar"></div>
                     <div className="bar"></div>
@@ -46,6 +54,8 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
 
 
 

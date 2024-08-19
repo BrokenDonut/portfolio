6ada7dfff18a1e4fffc8798801fa312bd1c22331
RonaldoChulluncuy <ronaldochulluncuy@gmail.com>
2024-08-19 00:17:06 -0500

    slider

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,10 +1,18 @@
-import ElitePage from './proyects/portadaElite.png'
-import TransportePage from './proyects/portadaTransporte.png'
-
+import Elite1 from './proyects/portadaElite.png'
+import Elite2 from './proyects/moreElite.png'
+import Elite3 from './proyects/moreElites.png'
+import Transport1 from './proyects/portadaTransporte.png'
+import Transport2 from './proyects/crudTransporte.png'
+import Transport3 from './proyects/loginTransporte.png'
 import './work.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-export default function Work(){
-    return(
+export default function Work() {
+    return (
         <div className="container-proyect" id="work">
             <div className="proyect-second">
                 <div className="proyect-title">
@@ -16,25 +24,58 @@ export default function Work(){
                 </div>
                 <div className="proyect-new">
                     <div className="program">
-                        <img src={ElitePage} alt="img"></img>
+                        <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Autoplay, Pagination, Navigation]}
+                            className="mySwiper">
+                            <SwiperSlide><img src={Elite1} alt="img"></img></SwiperSlide>
+                            <SwiperSlide><img src={Elite2} alt="img"></img></SwiperSlide>
+                            <SwiperSlide><img src={Elite3} alt="img"></img></SwiperSlide>
+                        </Swiper>
+                        
                         <div className="proyect-description">
                             <h2>Elite Page</h2>
                             <p>Realize un rediseño de una pagina de ventas de terreno para consigo poder realizar el buen uso de CSS.</p>
                             <button className="button-28">
                                 <a href="https://github.com/BrokenDonut/Elite" target="_blank">
-                                <i className="fab fa-github"></i> Code
+                                    <i className="fab fa-github"></i> Code
                                 </a>
                             </button>
                         </div>
                     </div>
                     <div className="program">
-                        <img src={TransportePage} alt="img"></img>
+                    <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            navigation={true}
+                            modules={[Autoplay, Pagination, Navigation]}
+                            className="mySwiper">
+                            <SwiperSlide><img src={Transport1} alt="img"></img></SwiperSlide>
+                            <SwiperSlide><img src={Transport2} alt="img"></img></SwiperSlide>
+                            <SwiperSlide><img src={Transport3} alt="img"></img></SwiperSlide>
+                        </Swiper>
                         <div className="proyect-description">
                             <h2>Transporte</h2>
                             <p>Usando Spring realize el diseño y la conexion de base de datos con MySQL.</p>
                             <button className="button-28">
                                 <a href="https://github.com/BrokenDonut/TransporteLogin" target="_blank">
-                                <i className="fab fa-github"></i> Code
+                                    <i className="fab fa-github"></i> Code
                                 </a>
                             </button>
                         </div>

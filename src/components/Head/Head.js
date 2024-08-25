20b7fc9fe357e11eb066c35c280b6be125b40d98
RonaldoChulluncuy <ronaldochulluncuy@gmail.com>
2024-08-25 18:02:43 -0500

    escala

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,14 +1,21 @@
 import Icon from './icon.jpeg'
 import Cv from './Ronaldo_Chulluncuy_CV.pdf'
 import './head.css'
+import Typewriter from 'typewriter-effect';
 
-export default function Head(){
-    return(
+export default function Head() {
+    return (
         <div className="head-container" id="perfil">
             <div className="head-second">
                 <div className="head-text">
                     <h3>Hola, mi nombre es Ronaldo &#128075;</h3>
-                    <h1>Full Stack</h1>
+                    <Typewriter
+                        options={{
+                            strings: ['Back End', 'Front End', 'Full Stack'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
                     <h3>Me Aficiona el diseño y creacion de aplicaiones web</h3>
                     <div className="head-botom">
                         <a href={Cv} download>
